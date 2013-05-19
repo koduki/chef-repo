9b9a65d3c697cbe4096e137fd8b9998e3e80e33f
koduki <pascalm3@gmail.com>
2013-05-19 12:41:07 +0900

    add base setting files.

---
+++ site-cookbooks/base_setting/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for base_setting
+
+This file is used to list changes made in each version of base_setting.
+
+## 0.1.0:
+
+* Initial release of base_setting
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ site-cookbooks/base_setting/recipes/default.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: base_setting
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# SSH setting
+service "sshd" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
+template '/etc/ssh/sshd_config' do
+  source  'etc/ssh/sshd_config.erb'
+  owner   'root'
+  group   'root'
+  mode    '0600'
+  notifies :reload, 'service[sshd]'
+end
+
+# Disable SELinux
+template '/etc/selinux/config' do
+  source  'etc/selinux/config'
+  owner   'root'
+  group   'root'
+  mode    '0644'
+end
+
+# zsh default setting
+template '/etc/skel/.zshrc' do
+  source  'etc/skel/dot_zshrc'
+  owner   'root'
+  group   'root'
+  mode    '0644'
+end
+
+template '/etc/skel/.zshrc.mine' do
+  source  'etc/skel/dot_zshrc.mine'
+  owner   'root'
+  group   'root'
+  mode    '0644'
+end
+
+
+# Disable unnecessary services
+%w{cpid apmd atd auditd autofs avahi-daemon avahi-dnsconfd bluetooth cups dhcdbd firstboot gpm hidd ip6tables iptables isdn mcstrans mdmonitor netfs nfslock pand pcscd portmap readahead_early restorecond rpcgssd rpcidmapd yum-updatesd xfs}.each do |service_name|
+  service service_name do
+    action [:disable, :stop]
+  end
+end
